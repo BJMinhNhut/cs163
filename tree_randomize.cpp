f3c3dad8e828b3247e3886036adbf071fc5ffe11
BJMinhNhut <nhutben678@gmail.com>
2023-07-04 11:13:33 +0700

    Implement input random generator

---
+++ tree_randomize.cpp
@@ -0,0 +1,16 @@
+#include "testlib.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	registerGen(argc, argv, 1);
+	freopen("tree_input.txt", "w", stdout);
+	int n = rnd.next(1, 20);
+	cout << n << '\n';
+	vector<int> a(n, 0);
+	for (int i = 0; i < n; ++i)
+		a[i] = rnd.next(1, 100);
+	println(a);
+	return 0;
+}
